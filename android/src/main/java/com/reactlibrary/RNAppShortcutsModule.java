b05e154c4a712f8e2cf47caf8f172a7c2daab6f7
lokyoung <lokyoung@outlook.com>
2017-03-30 19:36:24 +0800

    feat: add update judge exist of shortcut

--- android/src/main/java/com/reactlibrary/RNAppShortcutsModule.java
+++ android/src/main/java/com/reactlibrary/RNAppShortcutsModule.java
@@ -17,46 +17,124 @@ import com.facebook.react.bridge.Callback;
 
 public class RNAppShortcutsModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNAppShortcutsModule(ReactApplicationContext reactContext) {
-      super(reactContext);
-      this.reactContext = reactContext;
-  }
+    public RNAppShortcutsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-      return "RNAppShortcuts";
-  }
+    @Override
+    public String getName() {
+        return "RNAppShortcuts";
+    }
 
-  @ReactMethod
-  public void handleShortcut(Callback successCallback) {
-      Activity currentActivity = this.reactContext.getCurrentActivity();
-      String shortCutId = currentActivity.getIntent().getStringExtra("shortcutId");
-      if (shortCutId != null) {
-          successCallback.invoke(shortCutId);
-      }
-  }
+    @ReactMethod
+    public void handleShortcut(Callback successCallback) {
+        if (Build.VERSION.SDK_INT < 25) return;
 
-  @ReactMethod
-  public void setShortcut(String id, String shortLabel, String longLabel) {
-      Activity currentActivity = this.reactContext.getCurrentActivity();
-      Intent intent = new Intent(currentActivity.getApplicationContext(), MainActivity.class);
-      intent.putExtra("shortcutId", id);
-      intent.setAction(Intent.ACTION_VIEW);
+        Activity currentActivity = this.reactContext.getCurrentActivity();
+        String shortCutId = currentActivity.getIntent().getStringExtra("shortcutId");
+        if (shortCutId != null) {
+            successCallback.invoke(shortCutId);
+        }
+    }
 
-      if (Build.VERSION.SDK_INT < 25) return;
+    @ReactMethod
+    public void addShortcut(String id, String shortLabel, String longLabel, String iconFolderName, String iconName) {
+        if (Build.VERSION.SDK_INT < 25) return;
+        if (isShortcutExist(id)) return;
 
-      ShortcutManager shortcutManager = currentActivity.getSystemService(ShortcutManager.class);
+        ShortcutInfo shortcut = initShortcut(id, shortLabel, longLabel, iconFolderName, iconName);
 
-      ShortcutInfo shortcut = new ShortcutInfo.Builder(currentActivity, id)
-          .setShortLabel(shortLabel)
-          .setLongLabel(longLabel)
-          .setIcon(Icon.createWithResource(currentActivity.getApplicationContext(), R.drawable.icon))
-          .setIntent(intent)
-          .build();
+        ShortcutManager shortcutManager = getShortCutManager();
+        shortcutManager.addDynamicShortcuts(Arrays.asList(shortcut));
+    }
 
-      shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut));
-  }
+    @ReactMethod
+    public void removeShortcut(String id) {
+        if (Build.VERSION.SDK_INT < 25) return;
+
+        ShortcutManager shortcutManager = getShortCutManager();
+        shortcutManager.removeDynamicShortcuts(Arrays.asList(id));
+    }
+
+    @ReactMethod
+    public void removeAllShortcuts() {
+        if (Build.VERSION.SDK_INT < 25) return;
+
+        ShortcutManager shortcutManager = getShortCutManager();
+        shortcutManager.removeAllDynamicShortcuts();
+    }
+
+    @ReactMethod
+    public void exists(String id, Callback successCallback, Callback failureCallback) {
+        if (Build.VERSION.SDK_INT < 25) return;
+
+        if (isShortcutExist(id)) {
+            successCallback.invoke();
+        } else {
+            failureCallback.invoke();
+        }
+    }
+
+    @ReactMethod
+    public void updateShortcut(String id, String shortLabel, String longLabel, String iconFolderName, String iconName) {
+        if (Build.VERSION.SDK_INT < 25) return;
+
+        if (isShortcutExist(id)) {
+            ShortcutInfo shortcut = initShortcut(id, shortLabel, longLabel, iconFolderName, iconName);
+
+            ShortcutManager shortcutManager = getShortCutManager();
+            shortcutManager.updateShortcuts(Arrays.asList(shortcut));
+        } else {
+            return;
+        }
+    }
+
+    @Nullable
+    private ShortcutInfo initShortcut(String id, String shortLabel, String longLabel, String iconFolderName, String iconName) {
+        if (Build.VERSION.SDK_INT < 25) return null;
+
+        Activity currentActivity = this.reactContext.getCurrentActivity();
+        Intent intent = new Intent(currentActivity.getApplicationContext(), MainActivity.class);
+        intent.putExtra("shortcutId", id);
+        intent.setAction(Intent.ACTION_VIEW);
+
+        Context currentContext = currentActivity.getApplicationContext();
+        int iconId = currentContext.getResources().getIdentifier(iconName, iconFolderName, currentContext.getPackageName());
+
+        ShortcutInfo shortcut = new ShortcutInfo.Builder(currentActivity, id)
+                .setShortLabel(shortLabel)
+                .setLongLabel(longLabel)
+                .setIcon(Icon.createWithResource(currentActivity.getApplicationContext(), iconId))
+                .setIntent(intent)
+                .build();
+
+        return shortcut;
+    }
+
+    private boolean isShortcutExist(String id) {
+        if (Build.VERSION.SDK_INT < 25) return false;
+
+        ShortcutManager shortcutManager = getShortCutManager();
+        List<ShortcutInfo> shortcutInfoList = shortcutManager.getDynamicShortcuts();
+        for (ShortcutInfo shortcutInfo : shortcutInfoList) {
+            if (shortcutInfo.getId().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Nullable
+    private ShortcutManager getShortCutManager() {
+        if (Build.VERSION.SDK_INT < 25) return null;
+
+        Activity currentActivity = this.reactContext.getCurrentActivity();
+        ShortcutManager shortcutManager = currentActivity.getSystemService(ShortcutManager.class);
+
+        return shortcutManager;
+    }
 
 }
