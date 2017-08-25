f6fe34c6f789ffaf3d2be01d596d2983a8e97395
Elliot Hesp <elliot.hesp@gmail.com>
2017-08-25 12:20:43 +0100

    Remove createJSModules Override

--- android/src/main/java/com/rnas/RNAppShortcutsPackage.java
+++ android/src/main/java/com/rnas/RNAppShortcutsPackage.java
@@ -16,7 +16,6 @@ public class RNAppShortcutsPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new RNAppShortcutsModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
