4bc78b5b6adc24ded7cdde4230533f55548478ee
Carlito <CharlesMangwa@users.noreply.github.com>
2017-09-09 20:48:42 +0200

    Fix error introduced in exists() method

--- android/src/main/java/com/rnas/RNAppShortcutsModule.java
+++ android/src/main/java/com/rnas/RNAppShortcutsModule.java
@@ -75,7 +75,7 @@ public class RNAppShortcutsModule extends ReactContextBaseJavaModule {
         if (Build.VERSION.SDK_INT < 25) return;
 
         if (isShortcutExist(id)) {
-            promise.resolve(true);
+            promise.resolve(null);
         } else {
             promise.reject(SHORTCUT_NOT_EXIST, "Not found this app shortcut");
         }
